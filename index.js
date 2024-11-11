8c4b58ff85b2b6d843aca10974661a92c234a49c
3omar-github <70757645+3omar-github@users.noreply.github.com>
2024-11-11 14:04:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Watching over the Department", "Fighting Fires"];
+const statusMessages = ["Responding to alarms 🔥", "Bringing the heat, Dallas style! 🔥", "Educating the community about fire safety 🔥"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
